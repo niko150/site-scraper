bc743123c83ef688715e032c7b7efeb8fe176da9
Edgar Pino <me@edgarpino.com>
2017-02-09 23:23:31 -0600

    Added icons

--- tools/config.js
+++ tools/config.js
@@ -1,7 +1,6 @@
 
 module.exports = {
-  title: 'HTML Website Scraper',        // Your website title
+  title: 'Site Scraper',        // Your website title
   url: 'https://rsb.kriasoft.com',          // Your website URL
-  project: 'html_website_scraper',      // Firebase project. See README.md -> How to Deploy
   trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
 };
